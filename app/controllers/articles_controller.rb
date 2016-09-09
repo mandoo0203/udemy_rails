9cf2b29df068b58937e77aa4cd06df4a7c03b059
mandoo0203 <mandoo020374@gmail.com>
2016-09-10 03:30:48 +0900

    signup form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 
 
   def create
-    debugger
+
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
