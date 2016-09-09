a8e349aa3e40e6ce21a54a87e3b963a33026df11
mandoo0203 <mandoo020374@gmail.com>
2016-09-09 16:00:19 +0900

    rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+
+  root 'pages#home'
+
+
+  get 'about', to: 'pages#about'
+
+  resources :articles
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
