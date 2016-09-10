51792e66d3a999bf644bfe68f34c839a4bd61a6a
mandoo0203 <mandoo020374@gmail.com>
2016-09-10 14:55:10 +0900

    complete Authenticate system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if  user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "login is success"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "someting to wrong your account information"
+      render 'new'
+    end
+
+
+
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "logout"
+    redirect_to root_path
+  end
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
 
   end
-
 
   def new
    @user = User.new
@@ -21,11 +21,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "your accout is updated successfully"
       redirect_to articles_path
@@ -36,7 +34,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_article = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -47,5 +44,17 @@ class UsersController < ApplicationController
   def user_params
       params.require(:user).permit(:username, :email, :password)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "you can only edit  your account"
+      redirect_to root_path
+    end
+  end
+
 
 end
